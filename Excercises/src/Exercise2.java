a492f63bbbbfaa8d31034e53261e018ea829c866
Maciek <uzytkownik@kursGIT.pl>
2021-02-23 13:56:27 +0100

    First commit

---
+++ Excercises/src/Exercise2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Exercise2 {
+    public static void main(String[] args) {
+
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Please enter a number");
+        int a = userInput.nextInt();
+        String s ="Maciek";
+        System.out.println("You have entered: " + a + " and your name is " +s);
+    }
+}
