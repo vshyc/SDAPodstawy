82321f3e5907bf1a3c910cb64158c9d9304784d4
Maciek <maciej.fradczak@gmail.com>
2021-02-23 13:57:20 +0100

    pierwszy commit

---
+++ Excercises/src/Excercise3.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Excercise3 {
+    public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Please enter a number");
+        int a = userInput.nextInt();
+        System.out.println("Enter your name");
+        String s = userInput.next();
+        System.out.println("You have entered: " + a + " and your name is " + s);
+
+    }
+}
