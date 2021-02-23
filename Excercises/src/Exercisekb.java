187b328b88e3be42a6f972796a899ae41491e36d
Katarzyna <katarzyna91b@gmail.com>
2021-02-23 13:56:47 +0100

    first comit

---
+++ Excercises/src/Exercisekb.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Exercisekb {
+    public static void main(String[] args) {
+        Scanner userImput = new Scanner(System.in);
+
+        System.out.println("Please enter a number");
+
+        int a = userImput.nextInt();
+        System.out.println("Enter your name");
+        String s = userImput.next();
+        System.out.println("You have entered: " + a + "and your name is " + s);
+    }
+}
