f678c6d51325be20919787b9efd4451c2e82d165
rafal <test@test.pl>
2021-02-23 13:56:35 +0100

    pierwszy commit

---
+++ Excercises/src/exercise2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class exercise2 {
+    public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+    System.out.println("please enter number");
+    int a = userInput.nextInt();
+
+    System.out.println("you have entered " + a);
+
+
+    }
+}
