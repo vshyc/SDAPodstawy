6c8d7a1110a48a853d96422284eee14a59c6727a
Dariusz Szyca <dariusz_szyca@epam.com>
2021-02-23 14:41:26 +0100

    first comit

--- Excercises/src/Exercise2.java
+++ Excercises/src/Exercise2.java
@@ -3,11 +3,15 @@ import java.util.Scanner;
 public class Exercise2 {
 
     public static void main(String[] args) {
+
         Scanner userInput = new Scanner(System.in);
-        System.out.println("Please enter a number");
-        int a = userInput.nextInt();
-        System.out.println("Enter your name");
-        String s = userInput.next();
-        System.out.println("You have entered: " + a + " and your name is " + s );
+
+        System.out.println("Enter your a text");
+
+        String userText = userInput.next();
+
+        System.out.println("You have entered: "  + userText );
+
+
     }
 }
