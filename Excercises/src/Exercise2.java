30a926547f85d6610b1d178693251fb7669fde91
Dariusz Szyca <dariusz_szyca@epam.com>
2021-02-24 09:01:27 +0100

    first comit

--- Excercises/src/Exercise2.java
+++ Excercises/src/Exercise2.java
@@ -8,10 +8,22 @@ public class Exercise2 {
 
         System.out.println("Enter your a text");
 
-        String userText = userInput.next();
+        String userText = userInput.nextLine();
 
         System.out.println("You have entered: "  + userText );
+        System.out.println("Length of text is: " + userText.length());
+        System.out.println("Text is empty : " + userText.isEmpty());
+        System.out.println(userText.toLowerCase());
+        System.out.println(userText.toUpperCase());
+        System.out.println("The text contains your name: " + userText.toLowerCase().contains("darek"));
+        System.out.println(userText.toLowerCase().indexOf("darek"));
+        System.out.println(userText.toLowerCase().lastIndexOf("darek"));
+        System.out.println(userText.substring(5));
+        System.out.println(userText.toLowerCase().equals("darek"));
 
+        String[] table = userText.split(" ");
+        System.out.println(table[0]);
 
+        System.out.println(userText.replace(" ",""));
     }
 }
