e17002ee8842e1fa4b3d6cc1a1339062bfedfe1b
Ewelina Dikta <dikta.e@gmail.com>
2021-02-24 08:59:53 +0100

    Komit Eweliny

--- Excercises/src/Exercise2.java
+++ Excercises/src/Exercise2.java
@@ -1,15 +1,22 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Exercise2 {
     public static void main(String[] args) {
         Scanner userInput = new Scanner(System.in);
 
-        System.out.println("Please enter a number");
-        int a = userInput.nextInt();
+        System.out.println("Enter a name");
 
-        System.out.println("Please enter a name");
-        String s = userInput.next();
+        String userText = userInput.nextLine();
 
-        System.out.println("You have entered: " + a + " and your name is " + s + ". \n Lovely, right?");
+        System.out.println("Your name is " + userText );
+
+        System.out.println("Lenght of text is: " + userText.length() + " letters.");
+        System.out.println("Text is this field empty? " + userText.isEmpty());
+
+        System.out.println(userText.toLowerCase());
+        System.out.println(userText.toUpperCase());
+
+        System.out.println("The text contains your name: " + userText.toLowerCase().contains("darek"));
     }
 }
