0debd08ba83225a4e976cb7e082a81886065f420
Iwona Komorowska <ivo@gmail.com>
2021-02-24 15:37:33 +0100

    zad4

--- src/Exercise4.java
+++ src/Exercise4.java
@@ -4,21 +4,33 @@ public class Exercise4 {
 
     public static void main(String[] args) {
         Scanner userInput = new Scanner(System.in);
-        System.out.println("please enter A number");
+        System.out.println("Podaj pierwszą A liczbę całkowitą");
         int a = userInput.nextInt();
-        System.out.println("plesase enter B number");
+        System.out.println("Podaj drugą B liczbę całkowitą");
         int b = userInput.nextInt();
-
-        int sum = a+b;
-        int d = a-b;
-        int e = a*b;
-        int f = a/b;
-        double powerOf = Math.pow(a,b);
-        System.out.println("dodawanie -> a = " +a +" ,b = " + b + " wynik to: " + sum);
-        System.out.println("odejmowanie -> a = " +a +" ,b = " + b + " wynik to: " + d);
-        System.out.println("mnożenie -> a = " +a +" ,b = " + b + " wynik to: " + e);
-        System.out.println("dzielenie -> a = " +a +" ,b = " + b + " wynik to: " + f);
-        System.out.println("podniesienie do potęgi -> a ="+ a + ", podniesione do potęgi "+b+", daje wynik "+ powerOf);
-
+        System.out.println("Podaj działanie \n1.sum + \n2.diff - \n3.multi * \n4.div : \n5.exp ^ \n");
+        int action = userInput.nextInt();
+        int sum = a + b;
+        int diff = a - b;
+        int multi = a * b;
+        double div = a / b;
+        double exp = Math.pow(a, b);
+        switch (action) {
+            case 1:
+                System.out.println("dodawanie -> a = " + a + " ,b = " + b + " wynik to: " + sum);
+                break;
+            case 2:
+                System.out.println("odejmowanie -> a = " + a + " ,b = " + b + " wynik to: " + diff);
+                break;
+            case 3:
+                System.out.println("mnożenie -> a = " + a + " ,b = " + b + " wynik to: " + multi);
+                break;
+            case 4:
+                System.out.println("dzielenie -> a = " + a + " ,b = " + b + " wynik to: " + div);
+                break;
+            case 5:
+                System.out.println("podniesienie do potęgi -> a = " + a + " ,b = " + b + " wynik to: " + exp);
+                break;
+        }
     }
 }
