1285a96c38d2312863cc204de8f60a242d67abf7
Dymcia <karolinadymarek@wp.pl>
2021-02-27 12:54:38 +0100

    zadania 27.02.2021

---
+++ Excercises/src/Excersise2.java
@@ -0,0 +1,9 @@
+public class Excersise2 {
+    public static void main(String[] args) {
+        int i=1000000000;
+        long l=-90000000;
+        double d=100;
+        System.out.println(i+""+l+""+d+"");
+
+    }
+}
---
+++ Excercises/src/Excersise22.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Excersise22 {
+    public static void main(String[] args) {
+        Scanner userInput= new Scanner(System.in);
+        System.out.println("Please enter a number");
+        int a =userInput.nextInt();
+        String s=("Karolina");
+        System.out.println("You have entered:"+a+"and your name is "+s);
+    }
+}
